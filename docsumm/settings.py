aa6c5a3e45330058df3d7116e43ac08b96872f5f
Sreeram K <sreeramfireball@gmail.com>
2021-03-03 08:08:09 +0530

    removed exposed key

--- docsumm/settings.py
+++ docsumm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ma#x-!!brlo5ab=5fwjg*+8trz-3q+xg0p&fdia8(3b61qoc@v'
+SECRET_KEY = 'xx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
